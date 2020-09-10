627e1a27e55d31c7fcf6ee2c09f2e5e8113c5732
k.pawlonka <k.pawlonka@student.isartdigital.com>
2020-09-10 14:32:13 +0200

    set up the map in the map file

--- src/defs.h
+++ src/defs.h
@@ -1,9 +1,16 @@
 #ifndef __DEFS__
 #define __DEFS__
 
-#define WIDTH 800
-#define HEIGHT 600
+// 28 tiles width
+// 31 tiles height
 
+#define SCREEN_WIDTH 896
+#define SCREEN_HEIGHT 1088
+
+#define BOARD_WIDTH 28
+#define BOARD_HEIGHT 31
+
+#define UNIT_SIZE 16
 
 
 #define MAX_ENEMIES 4
