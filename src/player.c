a99313063a2c059e97945b9e6b54ce7818143b2e
k.pawlonka <k.pawlonka@student.isartdigital.com>
2020-09-11 17:00:24 +0200

    commit

--- src/player.c
+++ src/player.c
@@ -14,6 +14,9 @@ player_t* create_player()
 
     player->x = PLAYER_START_X;
     player->y = PLAYER_START_Y;
+	
+	player->destX = player->x;
+	player->destY = player->y;
 
     player->speed = PLAYER_SPEED;
 	
@@ -48,7 +51,7 @@ void player_update_destination(game_t* game)
         player->destX = x * UNIT_SIZE;
         player->destY = (y + 1) * UNIT_SIZE;
     }
-    else if (input->left && x > 0 && !board->cells[(x-1) + y * board->width].is_wall)
+    else if (input->left && x > 0 && !board->cells[(x - 1) + y * board->width].is_wall)
     {
         player->destX = (x - 1) * UNIT_SIZE;
         player->destY = y * UNIT_SIZE;
@@ -68,22 +71,22 @@ void player_move(game_t* game, float deltaTicks)
     player_t* player = game->player;
 
     // PLAYER IS ON DESTINATION
-    // if (player->x == player->destX && player->y == player->destY)
-    // {
+    if (player->x == player->destX && player->y == player->destY)
+    {
         player_update_destination(game);
-    // }
+    }
 
     float vel = player->speed * (deltaTicks / 1000.f);
 
     // ELSE WE MOVE FORWARD
     if (player->destX < player->x)
         player->x -= vel;
-    else
+    else if (player->destX > player->x)
         player->x += vel;
 
     if (player->destY < player->y)
         player->y -= vel;
-    else
+    else if (player->destY > player->y)
         player->x += vel;
     
 }
