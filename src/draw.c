a556d3d55d6677be17a2df319f627aa14227457a
k.pawlonka <k.pawlonka@student.isartdigital.com>
2020-09-12 00:24:52 +0200

    changed map cells render

--- src/draw.c
+++ src/draw.c
@@ -22,16 +22,19 @@ void draw_board(game_t* game)
 {
 	board_t* board = game->board;
 	
-	SDL_SetRenderDrawColor(game->draw->renderer, 255, 0, 0, 255);
-	
 	for (int i = 0; i < board->height; ++i)
 	{
 		for (int j = 0; j < board->width; ++j)
 		{
 			if(board->cells[j + i * board->width].is_wall)
 			{
+				SDL_SetRenderDrawColor(game->draw->renderer, 255, 0, 0, 255);
 				SDL_Rect rect = {j * UNIT_SIZE, i * UNIT_SIZE, UNIT_SIZE, UNIT_SIZE};
-				SDL_RenderDrawRect(game->draw->renderer, &rect);
+				SDL_RenderFillRect(game->draw->renderer, &rect);
+				
+				SDL_SetRenderDrawColor(game->draw->renderer, 0, 0, 0, 255);
+				SDL_Rect rect_ = {j * UNIT_SIZE, i * UNIT_SIZE, UNIT_SIZE, UNIT_SIZE};
+				SDL_RenderDrawRect(game->draw->renderer, &rect_);
 			}
 		}
 	}
