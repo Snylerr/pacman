9ce88aac96d353b29a4726473d710c3514f22e65
k.pawlonka <k.pawlonka@student.isartdigital.com>
2020-09-14 13:35:40 +0200

    fixed vec

--- src/sdl_utils.c
+++ src/sdl_utils.c
@@ -43,13 +43,16 @@ SDL_Texture* utils_load_texture(SDL_Renderer* renderer, SDL_Surface* surface)
 
 void utils_entity_render_cpy(game_t* game, void* entity)
 {
-	//player_t* entity_ = (player_t*)entity;
-	//tile_t* entity_tile = entity_->tile;
+	player_t* entity_ = (player_t*)entity;
+	tile_t* entity_tile = entity_->tile;
+	
+	int x = (int)entity_->pos->x;
+	int y = (int)entity_->pos->y;
 
-	//SDL_Rect src_rect = {entity_tile->width, entity_tile->height, entity_tile->width, entity_tile->height};
-	//SDL_Rect dst_rect = {entity_->x, entity_->y, 32, 32};
+	SDL_Rect src_rect = {entity_tile->width, entity_tile->height, entity_tile->width, entity_tile->height};
+	SDL_Rect dst_rect = {x, y, 32, 32};
 
-	//SDL_RenderCopy(game->draw->renderer, entity_->sprite, &src_rect, &dst_rect);
+	SDL_RenderCopy(game->draw->renderer, entity_->sprite, &src_rect, &dst_rect);
 }
 
 void utils_cell_render_cpy(game_t* game, board_t* board, cell_t* cell, int x, int y)
@@ -57,7 +60,7 @@ void utils_cell_render_cpy(game_t* game, board_t* board, cell_t* cell, int x, in
 	tile_t* cell_tile = cell->tile;
 
 	SDL_Rect src_rect = {cell_tile->column * cell_tile->width, cell_tile->row * cell_tile->height, cell_tile->width, cell_tile->height};
-	SDL_Rect dst_rect = {x, y, 32, 32};
+	SDL_Rect dst_rect = {x, y, UNIT_SIZE, UNIT_SIZE};
 
 	SDL_RenderCopy(game->draw->renderer, board->tileset, &src_rect, &dst_rect);
 }
